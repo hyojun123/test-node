145472c6e3bb6b88a00525e66aa3d6074d3d07e9
hyojun.choi <hyojun.choi@weddingbook.com>
2021-07-20 13:31:05 +0900

    .

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    console.log("request success");
+    res.send({name:'a',age:10});
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
